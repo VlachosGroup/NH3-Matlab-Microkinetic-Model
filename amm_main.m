27496f813aa78f05bf127b206252a1f5b0e1d153
Gerhard R Wittreich, P.E <wittregr@udel.edu>
2016-12-22 15:36:34 -0500

    CSTR refinements

--- amm_main.m
+++ amm_main.m
@@ -1,9 +1,9 @@
 clear
 global T T_ref T_pulse T_orig beta P V Q_in c_N2 c_H2 c_NH3 SDEN abyv c_tot pp
-T_orig = 675;
+T_orig = 600;
 T = T_orig;
 T_pulse = 975;
-P = 1.0;
+P = 1;
 pp=[0,P];
 beta = 1;
 tspan1 = 7473;
@@ -14,13 +14,13 @@ T_ref = 1; %K
 X_H2  = 0;
 X_N2  = 0;
 X_NH3 = 1;
-Y_H2 = X_H2/(X_H2+X_N2+X_NH3);
-Y_N2 = X_N2/(X_H2+X_N2+X_NH3);
+Y_H2  = X_H2 /(X_H2+X_N2+X_NH3);
+Y_N2  = X_N2 /(X_H2+X_N2+X_NH3);
 Y_NH3 = X_NH3/(X_H2+X_N2+X_NH3);
 V = 1;
 Q_in = 3.76e-3;
 SDEN = 3.1331e-10;             % Catalyst surface site density (moles/cm2)
-abyv = 100;
+abyv = 1000;
 cat_surf = abyv*V;
 MW_H = 1.007970;
 MW_N = 14.006700;
@@ -34,11 +34,11 @@ M_N2 = c_N2*V*(2*MW_N);
 M_NH3 = c_NH3*V*(3*MW_H+MW_N);
 options0 = odeset ('MaxStep',0.001,'NonNegative',[1 2 3 4 5 6 7 8 9],'BDF','on','InitialStep',1e-10,'Stats','off','AbsTol',1e-14,'RelTol',1e-12);
 options1 = odeset ('MaxStep',0.001,'NonNegative',[1 2 3 4 5 6 7 8 9],'BDF','on','InitialStep',1e-5,'Stats','off','AbsTol',1e-12,'RelTol',1e-10);
-options2 = odeset ('MaxStep',0.1,'NonNegative',[1 2 3 4 5 6 7 8 9],'BDF','on','Stats','off','AbsTol',1e-12,'RelTol',1e-10);
+options2 = odeset ('NonNegative',[1 2 3 4 5 6 7 8 9],'BDF','on','Stats','off','AbsTol',1e-12,'RelTol',1e-10);
 tic;
 %tspan = tspan1;
 s0 = [0 0 0 0 0 0 c_N2 c_H2 c_NH3];
-%[t,s] = ode15s(@ammonia,[0 tspan],s0,options0);
+%[t,s] = ode15s(@ammonia,[0 tspan],s0,options0)
 %s0 = s(end,1:9);
 %s(:,10) = SDEN - sum(s(:,2:6),2);
 %save('grw1.mat','t','s','-v7.3');
@@ -51,24 +51,42 @@ T_pulse = 875;
 %save('grw2.mat','t','s','-v7.3');
 %clear t s;
 T_pulse = 675;
-tspan = 1e3;%tspan1b - tspan;
+tspan = 1e6;%tspan1b - tspan;
 [t,s] = ode15s(@ammonia,[0 tspan],s0,options2);
 s0 = s(end,1:9);
 %s(:,10) = SDEN - sum(s(:,2:6),2);
 %save('grw3.mat','t','s','-v7.3');
 clear t s;
-T_pulse = 675;
-tspan = 1e2;%tspan2 - tspan;
-[t,s] = ode15s(@ammonia,[0 tspan],s0,options2);
+tstart = 0;
+pfrnodes = 1;
+for pfr=1:pfrnodes;
+T_pulse = 800;
+tspan = 1000;%tspan2 - tspan;
+[t,s] = ode15s(@ammonia,[tstart tspan+tstart],s0,options1);
+s0 = zeros(1,9);
+s0(7:9) = s(end,7:9);
 s(:,10) = SDEN - sum(s(:,2:6),2);
+tr{pfr}=t;
+sr{pfr}=s;
+tstart = t(end);
+c_N2 = s0(7);
+c_H2 = s0(8);
+c_NH3 = s0(9);
+end
 %save('grw4.mat','t','s','-v7.3');
 toc;
 %save('ammonia_dual_pulse_2.mat','s','t')
 figure(1)
-plot(t,s(:,[9 8 7])./sum(s(:,7:9),2),'-')
-legend('NH_3','H_2','N_2')
+hold on
+for pfr=1:pfrnodes
+plot(tr{pfr},sr{pfr}(:,7)./sum(sr{pfr}(:,7:9),2),'b')
+plot(tr{pfr},sr{pfr}(:,8)./sum(sr{pfr}(:,7:9),2),'r')
+plot(tr{pfr},sr{pfr}(:,9)./sum(sr{pfr}(:,7:9),2),'g')
+end
+hold off
+legend('N_2','H_2','NH_3')
 %xlim([tspan1 tspan2]);
-fs=s(end,7:9)./sum(s(end,7:9),2)
+fs=sr{pfrnodes}(end,7:9)./sum(sr{pfrnodes}(end,7:9),2)
 figure(2)
 plot(t,s(:,[1 3 4 10])./(SDEN),'-')
 legend('\theta_*','H_*','NH_{3*}','N_{2*}')
